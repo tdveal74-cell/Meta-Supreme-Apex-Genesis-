47e9e7ea2af697c6a1eeaff22cd993bca2c8240d
tdveal74-cell <tdveal74@gmail.com>
2026-08-26 02:01:07 -0400

    Wire live capability mesh into command center

--- apps/web/app/command-center/page.tsx
+++ apps/web/app/command-center/page.tsx
@@ -1,5 +1,11 @@
+import { CapabilityDock } from "@/components/command-center/CapabilityDock";
 import { UnifiedCommandCenter } from "@/components/command-center/UnifiedCommandCenter";
 
 export default function CommandCenterPage() {
-  return <UnifiedCommandCenter />;
+  return (
+    <>
+      <UnifiedCommandCenter />
+      <CapabilityDock />
+    </>
+  );
 }
