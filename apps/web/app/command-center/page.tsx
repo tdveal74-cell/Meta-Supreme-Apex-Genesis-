9f9d2da329ce75bffff8c6d887beebf8f0f27b4c
tdveal74-cell <tdveal74@gmail.com>
2026-08-26 02:06:25 -0400

    Add live Mission Control advisor to unified cockpit

--- apps/web/app/command-center/page.tsx
+++ apps/web/app/command-center/page.tsx
@@ -1,10 +1,12 @@
 import { CapabilityDock } from "@/components/command-center/CapabilityDock";
+import { MissionAdvisor } from "@/components/command-center/MissionAdvisor";
 import { UnifiedCommandCenter } from "@/components/command-center/UnifiedCommandCenter";
 
 export default function CommandCenterPage() {
   return (
     <>
       <UnifiedCommandCenter />
+      <MissionAdvisor />
       <CapabilityDock />
     </>
   );
